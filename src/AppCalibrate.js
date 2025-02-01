444f64996efa175ac18f3d7524d5584351aa6f9d
Kyle Keating <kkeating@gmail.com>
2025-01-31 21:45:52 -0800

    Adjust xhairs, add color from GG

--- src/AppCalibrate.js
+++ src/AppCalibrate.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import styles from './css/AppCalibrate.module.css'
 import {ReactComponent as Close} from './assets/xmark-solid.svg';
+import  { getMessageResult } from './Utils'
 
 var crosshairs = require('./electron/crosshairs');
+
 const electron = window.require('electron');
 const ipcRenderer  = electron.ipcRenderer;
-
-
 
 export default function AppCalibrate() {
   
   const canvasRef = useRef(null);
-  const [showCrosshairs, setShowCrosshairs] = useState(false);
-
+  
   var prevTime = 0;
   var renderQueue = []; // might be state, not sure
   var xhairs;
@@ -23,29 +22,30 @@ export default function AppCalibrate() {
     document.addEventListener('keydown', handleClick);
 
     document.getElementById("canvas").style.display = '';
-    if (renderQueue.length === 0){
-      // first time only;
-      initXhairs();
-    }
+
+    
+    loadMainLedFromGG();
 
     return ()=>{
       document.removeEventListener("keydown", handleClick);
       document.getElementsByTagName('body')[0].classList.remove(styles.bodyCalibrate)
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   function close() {
     window.close();
   }
+
   function handleClick(event) {
     if (event.key === 'Escape' || event.keyCode === 27) {
         close();
       }
   }
 
-  function initXhairs() {
+  function initXhairs(color) {
     const ctx = canvasRef.current.getContext("2d");
-    xhairs = new crosshairs.Crosshairs(ctx);
+    xhairs = new crosshairs.Crosshairs(ctx, color);
     renderQueue.push(xhairs);
     window.requestAnimationFrame(render.bind(this, ctx, renderQueue, 0));
   }
@@ -61,9 +61,41 @@ export default function AppCalibrate() {
     });
     ctx.restore();
     prevTime = time;
-    //console.log('render ',time);
     window.requestAnimationFrame(render.bind(this, ctx, renderQueue, prevTime));
-    
+  }
+
+  // copied from AppGaimglass
+  function parseMainLedFromGG(message) { 
+    const params = message.split('&');
+    let c = {};
+    //let led;
+    params.forEach(param => {
+      const [key, value] = param.split('=');
+      // eslint-disable-next-line default-case
+      switch(key) {
+        case 'color':
+          const [r,g,b] = value.split(',');
+          c.r = Number(r);
+          c.g = Number(g);
+          c.b = Number(b);
+          break;
+        /*case 'ledOn':
+          led = Boolean(Number(value));
+          break;*/
+        case 'brightness':
+          c.a = Number(value);
+          break;
+      }
+    });
+    return c;
+  }
+  
+  // copied from AppGaimglass
+  async function loadMainLedFromGG() { 
+    getMessageResult(ipcRenderer.sendSync('get-led-state'), (result) => {
+      const color = parseMainLedFromGG(result);
+      initXhairs(color);
+    });
   }
 
   return (
